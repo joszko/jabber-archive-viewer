639ab5607d8055c29d569c0f2491bfcbf3a48032
joszko <joszko@gmail.com>
2017-04-15 12:29:51 +0200

    updated the formatting of HTML file

--- viewer_webbrowser.py
+++ viewer_webbrowser.py
@@ -11,8 +11,20 @@ os.chdir(profile + path)
 
 # os.chdir(r'C:\Users\u8008701\Desktop\test')
 
-time_formatting = '<span style="font-family:Segoe UI;color:#000000;font-size:10pt;font-weight:normal;font-style' \
-                  ':normal;text-decoration:none;"> '
+time_formatting = '<span style="font-family:Segoe UI;color:#000000;font-size:10pt;font-weight:bold;font-style:normal' \
+                  ';text-decoration:none;"> '
+
+date_separator_formatting = '<div><span style="font-family:Segoe ' \
+                            'UI;color:#000000;font-size:12pt;font-weight:bolder;font-style:normal;text-decoration' \
+                            ':none;"> '
+
+sender_formatting = '<span style="font-family:Segoe UI;color:#000000;font-size:10pt;font-weight:bold;font-style' \
+                    ':normal;text-decoration:none;"> '
+
+header = '<!doctype html><html lang="en"><head><meta charset="utf-16"><title>Your Cisco Jabber ' \
+         'Archive</title></head><body> '
+
+footer = '</body></html>'
 
 for file in glob.glob("*.db"):
     conn = sqlite3.connect(file)
@@ -22,13 +34,30 @@ for file in glob.glob("*.db"):
     conn.close()
 
     with open('archive.html', 'wb') as html:
+        html.write(header.encode('UTF-16'))
+        date_check = None
         for row in rows:
-            time_string = time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime(int(row[0] / 1000000)))
+            date_separator = date_separator_formatting + time.strftime("%a, %d %b %Y", time.localtime(int(row[0] / 1000000))) + '<span></div><hr/>'
+
+            if date_check != time.strftime("%a, %d %b %Y", time.localtime(int(row[0] / 1000000))):
+                html.write(date_separator.encode('UTF-16'))
+
+            time_string = time.strftime("%H:%M:%S", time.localtime(int(row[0] / 1000000)))
+
             message = row[1].replace('<div>', '').replace('</div>', '')
-            sender = time_formatting + ' ' + row[2] + ' </span>'
-            full_line = '<div>' + time_formatting + time_string + ': </span> ' + sender + ' - ' + message + '</div>' + '\n '
+
+            sender_string = row[2][:-19].replace('.', ' ').title()
+
+            sender = sender_formatting + ' ' + sender_string + ' </span>'
+
+            full_line = '<div>' + time_formatting + time_string + ' - </span> ' + sender + ' - ' + message + '</div>' + '\n '
+
             html.write(full_line.encode('UTF-16'))
 
+            date_check = time.strftime("%a, %d %b %Y", time.localtime(int(row[0] / 1000000)))
+
+        html.write(footer.encode('UTF-16'))
 
 url = profile + path + '\\' + 'archive.html'
+# url = 'archive.html'
 webbrowser.open_new_tab(url)
